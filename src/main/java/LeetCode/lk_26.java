14b48eec8649d13d1b70b4b78a9476677ecb9fa1
gjkt <2910223554@qq.com>
2023-12-29 11:25:49 +0800

    力扣26

--- src/main/java/LeetCode/lk_26.java
+++ src/main/java/LeetCode/lk_26.java
@@ -1,20 +1,38 @@
 package LeetCode;
 
+import java.util.Arrays;
+
+/**
+ * 删除有序数组中的重复项
+ */
 public class lk_26 {
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{1,1,2,2}));
+        System.out.println(new lk_26().removeDuplicates(new int[]{1, 1, 2}));
     }
 
-    public static int removeDuplicates(int[] nums) {
-        int p = 1;// 指针1
-        int q = 0;// 指针2
+    public int removeDuplicates(int[] nums) {
         int len = nums.length;
-        while (p < len) {
-            if (nums[p] != nums[q]) {
-                nums[++q] = nums[p];
+        int left = 0;
+        for (int right = left + 1; right < len; right++) {
+            if (nums[right] != nums[left]) {
+                left++;
+                nums[left] = nums[right];
             }
-            p++;
         }
-        return q + 1;
+//        Arrays.stream(nums).forEach(System.out::println);
+        return left+1;
     }
+
+//    public int removeDuplicates(int[] nums) {
+//        int p = 1;// 指针1
+//        int q = 0;// 指针2
+//        int len = nums.length;
+//        while (p < len) {
+//            if (nums[p] != nums[q]) {
+//                nums[++q] = nums[p];
+//            }
+//            p++;
+//        }
+//        return q + 1;
+//    }
 }
