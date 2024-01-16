9b01d50e338bd0eea99415d7bcc715b2b93ac878
gjkt <2910223554@qq.com>
2024-01-16 10:49:40 +0800

    力扣141

---
+++ src/main/java/LeetCode/lk_141.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * @author Gjkt
+ * @description
+ * @since 2024/1/16 10:41
+ */
+public class lk_141 {
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            ListNode p = head;
+            // 快慢指针相遇就是有环
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
